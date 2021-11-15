ca4d8a8d97f803ade48ebea86995cfbe4294dec7
Aram Plaza Bel <arampb84@gmail.com>
2021-11-16 00:38:38 +0100

    Primer enemic
    Tot amontonat i mal posat

---
+++ Assets/Scripts/Enemy_behaviour.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_behaviour : MonoBehaviour
+{
+
+    #region public variables
+    public Transform raycast;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+    public float attackDistance; //Minimum distance for attack
+    public float moveSpeed;
+    public float timer;  //Timer cooldown between attacks
+    #endregion
+
+    #region private variables
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator anim;
+    private float distance; //distance b/w enemy and player
+    private bool attackMode;
+    private bool inRange; //Check if Player is in range
+    private bool cooling; //Check if Enemy is cooling after attack
+    private float intTimer;
+    #endregion
+
+    private void Awake()
+    {
+        intTimer = timer; //initial value of timer
+        anim = GetComponent<Animator>(); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (inRange)
+        {
+            hit = Physics2D.Raycast(raycast.position, Vector2.left, rayCastLength, raycastMask);
+            RaycastDebugger();
+        }
+
+        //When Player is detected
+        if (hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        else if(hit.collider == null)
+        {
+            inRange = false;
+        }
+        
+        if(inRange == false)
+        {
+            anim.SetBool("CanWalk",false);
+            StopAttack();
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        if(distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+        else if(attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+
+        if (cooling)
+        {
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    void Move()
+    {
+        anim.SetBool("CanWalk", true);
+
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Enemy_attack"))
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void Attack()
+    {
+        timer = intTimer; // Reset Timer when Player enter attack range
+        attackMode = true; // To check if Enemy can still attack or not
+
+        anim.SetBool("CanWalk", false);
+        anim.SetBool("Attack", true);
+    }
+
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+        anim.SetBool("Attack", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger");
+        if (collision.gameObject.tag == "Player")
+        {
+            target = collision.gameObject;
+            inRange = true;
+        }
+    }
+
+    void RaycastDebugger()
+    {
+        if (distance > attackDistance)
+        {
+
+            Debug.DrawRay(raycast.position, Vector2.left * rayCastLength, Color.red);
+        }
+        else if(attackDistance > distance)
+        {
+            Debug.DrawRay(raycast.position, Vector2.left * rayCastLength, Color.green);
+        }
+    }
+}
---
+++ Assets/Sprites/enemy_base/BasicEnemyController.cs
@@ -0,0 +1,251 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour
+{
+    private enum State
+    {
+        Walking,
+        Knockback,
+        Dead
+    }
+
+    private State currentState;
+
+    [SerializeField]
+    private float
+        groundCheckDistance,
+        wallCheckDistance,
+        movementSpeed,
+        maxHealth,
+        knockbackDuration;
+
+    [SerializeField]
+    private Transform
+        groundCheck,
+        wallCheck;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    [SerializeField]
+    private Vector2 knockbackSpeed;
+
+    [SerializeField]
+    private GameObject
+        hitParticle,
+        deathChunkParticle,
+        deathBloodParticle;
+
+    private int 
+        facingDirection,
+        damageDirection;
+
+    private Vector2 movement;
+
+    private float 
+        currentHealth,
+        knockbackStartTime;
+
+    private bool 
+        groundDetected,
+        wallDetected;
+
+    private GameObject alive;
+
+    private Rigidbody2D aliveRb;
+
+    private Animator aliveAnim;
+
+    public bool takedamage = false;//PROVA
+    [SerializeField]
+    private GameObject player;//PROVA
+
+    [SerializeField]
+    private ParticleSystem particleDamage;//PROVA
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alive = transform.Find("Alive").gameObject;
+        aliveRb = alive.GetComponent<Rigidbody2D>();
+        aliveAnim = alive.GetComponent<Animator>();
+
+        facingDirection = 1;
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.Walking:
+                UpdateWalkingState();
+                break;
+            case State.Knockback:
+                UpdateKnockbackState();
+                break;
+            case State.Dead:
+                UpdateDeadState();
+                break;
+        }
+        if (Input.GetKeyDown("p"))
+        {
+            takedamage = true;
+        }
+        if (takedamage)
+        {
+            
+            float[] f = new float[2];
+
+            f[0] = 10;
+            f[1] = player.transform.position.x;
+            takedamage = false;
+            Damage(f);
+
+        }
+    }
+
+    //---------WALKING---------------
+    private void EnterWalkingState()
+    {
+
+    }
+
+    private void UpdateWalkingState()
+    {
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+        if(!groundDetected || wallDetected)
+        {
+          
+            Flip();
+        }
+        else
+        {
+            movement.Set(movementSpeed * facingDirection, aliveRb.velocity.y);
+            aliveRb.velocity = movement;
+        }
+    }
+
+    private void ExitWalkingState()
+    {
+
+    }
+
+    //---------KNOCKBACK---------------
+    private void EnterKnockbackState()
+    {
+        knockbackStartTime = Time.time;
+        movement.Set(knockbackSpeed.x * damageDirection, knockbackSpeed.y);
+        aliveRb.velocity = movement;
+        aliveAnim.SetBool("Knockback", true);
+    }
+
+    private void UpdateKnockbackState()
+    {
+        if (Time.time >= knockbackStartTime + knockbackDuration)
+        {
+            SwitchState(State.Walking);
+        }
+    }
+
+    private void ExitKnockbackState()
+    {
+        aliveAnim.SetBool("Knockback", false);
+    }
+    //---------DEAD---------------
+    private void EnterDeadState()
+    {
+        //Spawn chunks and blood
+        Instantiate(deathChunkParticle, alive.transform.position, deathChunkParticle.transform.rotation);
+        Instantiate(deathBloodParticle, alive.transform.position, deathBloodParticle.transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void UpdateDeadState()
+    {
+
+    }
+
+    private void ExitDeadState()
+    {
+
+    }
+
+
+    //----ALTRES----
+
+    private void Damage(float[] attackDetails)
+    {
+        currentHealth -= attackDetails[0];
+
+        Instantiate(hitParticle, alive.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+        particleDamage.Play();
+        if (attackDetails[1] > alive.transform.position.x)
+        {
+            damageDirection = -1;
+        }
+        else
+        {
+            damageDirection = 1;
+        }
+
+        //Hir particle
+
+        if (currentHealth > 0.0f)
+        {
+            SwitchState(State.Knockback);
+        }
+        else if (currentHealth <= 0.0f)
+        {
+            SwitchState(State.Dead);
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        alive.transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void SwitchState(State state)
+    {
+        switch (currentState)
+        {
+            case State.Walking:
+                ExitWalkingState();
+                break;
+            case State.Knockback:
+                ExitKnockbackState();
+                break;
+            case State.Dead:
+                ExitDeadState();
+                break;
+        }
+
+        switch (state)
+        {
+            case State.Walking:
+                EnterWalkingState();
+                break;
+            case State.Knockback:
+                EnterKnockbackState();
+                break;
+            case State.Dead:
+                EnterDeadState();
+                break;
+        }
+
+        currentState = state;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+}

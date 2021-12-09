5db62faf426fb62d25e49fcea325de22bc627efc
Aram Plaza Bel <arampb84@gmail.com>
2021-12-09 12:32:37 +0100

    More changes Shooter
    Knockback, damage, death and others adjustments

--- Assets/Scripts/Enemy_shooter.cs
+++ Assets/Scripts/Enemy_shooter.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class Enemy_shooter : MonoBehaviour
 {
-    public float speed;
-    public float lineOfSite;
-    public float shootingRange;
+
+    public int damageToPlayer = 1;
+    public int pointsToGive = 10;
+
+    public float
+        speed,
+        speedKnockback,
+        lineOfSite,
+        shootingRange,
+        maxHealth,
+        knockbackDuration;        
     public float fireRate = 1;
-    private float nextFireTime;
+
     public GameObject bullet;
     public GameObject bulletParent;
     private Transform player;
+    private float 
+        currentHealth,
+        nextFireTime,
+        knockbackStartTime;
+
+    [SerializeField]
+    private GameObject
+        hitParticle,
+        deathChunkParticle,
+        deathBloodParticle;
+
+    [SerializeField]
+    private ParticleSystem particleDamage;
+
+    private int 
+        damageDirectionX,
+        damageDirectionY;
+
+    private bool isKnockback = false;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -32,6 +60,70 @@ public class Enemy_shooter : MonoBehaviour
             Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
             nextFireTime = Time.time + fireRate;
         }
+        if (isKnockback)
+        {
+            Knockback();
+        }
+    }
+    public void Damage(float[] attackDetails)
+    {
+
+        currentHealth -= attackDetails[0];
+
+        //Instantiate(hitParticle, alive.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+        particleDamage.Play();
+
+        if (attackDetails[1] > transform.position.x)
+        {
+            damageDirectionX = -1;
+        }
+        else
+        {
+            damageDirectionX = 1;
+        }
+
+        if (attackDetails[2] > transform.position.y)
+        {
+            damageDirectionY = -1;
+        }
+        else
+        {
+            damageDirectionY = 1;
+        }
+
+
+        //Hit particle
+
+        if (currentHealth > 0.0f)
+        {
+            Knockback();
+        }
+        else if (currentHealth <= 0.0f)
+        {
+            Dead();
+            GameManager.Instance.addPoints(pointsToGive);
+        }
+    }
+    public void applyKnockback()
+    {
+        Knockback();
+    }
+    private void Knockback()
+    {
+        knockbackStartTime = Time.time;
+        isKnockback = true;
+        transform.position = transform.position + new Vector3(damageDirectionX, damageDirectionY, 0.0f) * speedKnockback;
+        if (Time.time >= knockbackStartTime + knockbackDuration)
+        {
+            isKnockback = false;
+        }
+    }
+    private void Dead()
+    {
+        //Spawn chunks and blood
+        Instantiate(deathChunkParticle, transform.position, deathChunkParticle.transform.rotation);
+        Instantiate(deathBloodParticle, transform.position, deathBloodParticle.transform.rotation);
+        Destroy(gameObject);
     }
     private void OnDrawGizmos()
     {

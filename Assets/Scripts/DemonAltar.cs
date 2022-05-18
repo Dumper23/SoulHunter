db7dd4fef79582ba6285c758cc619265285b6565
Dumper23 <carlostubes1@gmail.com>
2022-05-18 15:25:42 +0200

    Demon altar aditional info

--- Assets/Scripts/DemonAltar.cs
+++ Assets/Scripts/DemonAltar.cs
@@ -8,12 +8,14 @@ public class DemonAltar : MonoBehaviour
     public Animator demonLight;
     public ParticleSystem particle;
 
+    public GameObject indication;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             GameManager.Instance.playerInDemonicAltar = true;
+            indication.SetActive(true);
             demonLight.Play("turnOn");
             particle.Play();
         }
@@ -24,6 +26,7 @@ public class DemonAltar : MonoBehaviour
         if (collision.tag == "Player")
         {
             GameManager.Instance.playerInDemonicAltar = false;
+            indication.SetActive(false);
             demonLight.Play("turnOff");
             particle.Stop();
         }
